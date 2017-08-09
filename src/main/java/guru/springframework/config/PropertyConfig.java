827be053ad45b2f8d3f33407f54da9c7c9592ebf
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-08-09 15:02:31 +0800

    utilizing default spring boot application.properties configuration

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -4,14 +4,10 @@
  */
 package guru.springframework.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
@@ -22,11 +18,12 @@ import guru.springframework.examplebeans.FakeJmsBroker;
 @Configuration
 // @PropertySource({ "classpath:datasource.properties",
 // "classpath:jms.properties" })
-@PropertySources({ @PropertySource("classpath:datasource.properties"), @PropertySource("classpath:jms.properties") })
+// @PropertySources({ @PropertySource("classpath:datasource.properties"),
+// @PropertySource("classpath:jms.properties") })
 public class PropertyConfig {
 
-	@Autowired
-	Environment env;
+	// @Autowired
+	// Environment env;
 
 	@Value("${guru.username}")
 	String user;
