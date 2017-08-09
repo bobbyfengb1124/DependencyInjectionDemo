024a22f78e8cc51138e5f3222387f3db52a11a74
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-08-09 11:51:01 +0800

    ultilize environment variables

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -4,11 +4,13 @@
  */
 package guru.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
 
@@ -18,6 +20,9 @@ import guru.springframework.examplebeans.FakeDataSource;
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+	@Autowired
+	Environment env;
 
 	@Value("${guru.username}")
 	String user;
@@ -31,7 +36,8 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		// fakeDataSource.setUser(user);
+		fakeDataSource.setUser(env.getProperty("GURU_USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 
